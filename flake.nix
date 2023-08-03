8e316157a8d6d19a4133d8f6abf8a9b79b8548dc
Ethan Brooks <ethanabrooks@gmail.com>
2023-08-03 19:40:57 -0400

    minimize dependencies

--- flake.nix
+++ flake.nix
@@ -47,52 +47,12 @@
         dm-alchemy = pyprev.dm-alchemy.overridePythonAttrs (old: {
           buildInputs = old.buildInputs ++ (with pyfinal; [setuptools grpcio-tools]);
         });
-        grpcio-tools = pyprev.grpcio-tools.overridePythonAttrs (old: {
-          buildInputs = [pyfinal.grpcio pkgs.gcc-unwrapped];
-        });
-        mujoco-py =
-          (pyprev.mujoco-py.override {
-            preferWheel = false;
-          })
-          .overridePythonAttrs (old: {
-            env.NIX_CFLAGS_COMPILE = "-L${pkgs.mesa.osmesa}/lib";
-            preBuild = with pkgs; ''
-              export MUJOCO_PY_MUJOCO_PATH="${mujoco}"
-              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib
-            '';
-            buildInputs =
-              old.buildInputs
-              ++ [pyfinal.setuptools]
-              ++ (with pkgs; [mesa libGL]);
-            patches = [./mujoco-py.patch];
-          });
         torch = pyprev.pytorch-bin.overridePythonAttrs (old: {
           src = pkgs.fetchurl {
             url = "https://download.pytorch.org/whl/cu116/torch-1.13.1%2Bcu116-cp39-cp39-linux_x86_64.whl";
             sha256 = "sha256-20V6gi1zYBO2/+UJBTABvJGL3Xj+aJZ7YF9TmEqa+sU=";
           };
         });
-        torchrl = pyprev.buildPythonPackage {
-          pname = "torchrl";
-          version = "0.0.5";
-          propagatedBuildInputs = with pyfinal; [
-            tensordict
-          ];
-          nativeBuildInputs = with pyfinal; [
-            setuptools
-            cloudpickle
-            packaging
-            pkgs.which
-          ];
-          src = pkgs.fetchgit {
-            url = "https://github.com/pytorch/rl";
-            rev = "48eca9890e24a64b0f3ee133a74abafad7cd0768";
-            sha256 = "sha256-ZLHqKRO7O4ZOSPqtwVmyFElpRnEbhX97Iy8PkGh4Fqg=";
-            name = "torchrl";
-          };
-          doCheck = false;
-          patches = [./torchrl.patch];
-        };
       };
       poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
         python = pkgs.python39;
